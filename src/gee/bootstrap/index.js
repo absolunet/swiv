72985b3703c73624d4c3a8c279df5d8379b64d53
ABSOLUNET\tparent <tparent@absolunet.com>
2018-06-11 14:34:53 -0400

    Remove GA ID check

--- src/gee/bootstrap/index.js
+++ src/gee/bootstrap/index.js
@@ -1,27 +1,8 @@
 const configs = require('./../config');
 const debounceUntil = require('./../utils/debounce-until');
 const isGaInitialized = require('./../utils/is-ga-initialized');
-const NoTrackingIdError = require('./../error/no-tracking-id');
-const getGaId = require('./../utils/get-ga-id');
-
-const resolveTrackingId = () => {
-    if (!isGaInitialized(configs)){
-        return;
-    }
-
-    if (!configs.get('id')) {
-        configs.set('id', getGaId(configs));
-    }
-
-    if (!configs.has('id')) {
-        throw new NoTrackingIdError();
-    }
-
-    window[configs.ga]('create', configs.id);
-};
 
 debounceUntil(() => {
-    resolveTrackingId();
     window[configs.ga]('require', 'ec');
     configs.set('ready', true);
 }, () => {
--- src/gee/error/no-tracking-id.js
+++
@@ -1,7 +0,0 @@
-module.exports = class NoTrackingIdError extends Error {
-
-    constructor(message = '') {
-        const defaultMessage = 'Google Enhanced Ecommerce plugin requires a tracker ID to work. Set the ID witin the factory configuration (swiv.gee.configs.set("id", "UA-XXXXXXXXX-Y")).';
-        super(`${defaultMessage}${(message ? ` ${message}` : '')}`);
-    }
-};
